969bb9ca6d1d9e424334d6adc525bbd51970042c
User <User@DESKTOP-F4157Q9>
2020-06-12 17:17:12 -0500

    First Commit....

---
+++ src/Javaclass2.java
@@ -0,0 +1,11 @@
+public class Javaclass2 {
+
+	public static void main(String[] args) {
+		
+
+		System.out.println(10+20);
+		System.out.println("Welcome");
+		System.out.println("Hi my name is Farha");
+	}
+
+}
---
+++ src/datatypes.java
@@ -0,0 +1,22 @@
+public class datatypes {
+
+	public static void main(String[] args) {
+		
+	//int a; //declaration
+	
+	//a=100;  //assignment
+	
+	
+	      int a=100;  //declration + assignment
+          double d=234.123;
+          char c='A';
+          boolean b=true;
+          String s="welcome";
+          
+    		
+		System.out.println(a);
+		System.out.println(d);
+		System.out.println(c);
+		System.out.println(b);
+		System.out.println(s);
+		
